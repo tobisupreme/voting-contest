cc3ecf702cccebf5f98b65019acc577e1a8a5722
Tobi Balogun <98078707+tobisupreme@users.noreply.github.com>
2023-09-02 05:30:17 +0100

    new: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerOptions = new DocumentBuilder()
+    .setTitle('Law School Voting API')
+    .setDescription('Vote!')
+    .setVersion('0.1')
+    .addBearerAuth()
+    .build();
+
+  const swagDocument = SwaggerModule.createDocument(app, swaggerOptions);
+  SwaggerModule.setup('swagger', app, swagDocument);
+
   await app.listen(3000);
 }
 bootstrap();
